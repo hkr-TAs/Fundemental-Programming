4e641693be712c28d4f0ca0a4fd4905e81220f01
Adis Veletanlic <adis.veletanlic@gmail.com>
2022-09-20 20:34:37 +0200

    add shift solutions

--- H2_polya&flowchart/reversenumbers.py
+++ H2_polya&flowchart/reversenumbers.py
@@ -1,16 +1,16 @@
-"""Bob has a list of binary numbers and he needs help reversing them all. Bob would like to have it printed on the screen."""
+"""Bob has a list of binary binarys and he needs help reversing them all. Bob would like to have it printed on the screen."""
 
-sample_number = ["10101010", "01011011", "10101000", "01001101", "10101110"]
+binary_numbers = ["10101010", "01011011", "10101000", "01001101", "10101110"]
 
 
 ## Solution with for loop
-for number in sample_number:
-    print(number[::-1])
+for binary in binary_numbers:
+    print(binary[::-1])
 
 
 ## Solution with while loop
 count = 0
-while count != len(sample_number):
-    reversed_binary = sample_number[count][::-1]
+while count != len(binary_numbers):
+    reversed_binary = binary_numbers[count][::-1]
     print(reversed_binary)
     count += 1
